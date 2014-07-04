ef15a7dbd5c0ed2e35b7c67b9661098622d25e99
etna-scorpio <etna.scorpio@gmail.com>
2014-07-04 17:22:01 +0300

    order details page is added

--- js/common.js
+++ js/common.js
@@ -4,4 +4,12 @@ head.ready(function() {
 		$(this).children().toggleClass('active');
 	});
 
+	$('.dropdown').change(function(event) {
+		$(this).next().children('.slct').text($(this).val());
+	});
+
+	$('.remove').click( function(){
+		$(this).closest('tr').fadeOut('slow');
+	});
+
 });
