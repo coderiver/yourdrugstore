e891b94c6a793626c99ec087b86a36fd3035b8d5
etna-scorpio <etna.scorpio@gmail.com>
2014-07-09 14:08:54 +0300

    checkout payment is added

--- js/common.js
+++ js/common.js
@@ -8,6 +8,10 @@ head.ready(function() {
 		$(this).next().children('.slct').text($(this).val());
 	});
 
+	$('.dropdown select').change(function(event) {
+		$(this).next().val($(this).val());
+	});
+
 	$('.remove').click( function(){
 		$(this).closest('tr').fadeOut('slow');
 	});
